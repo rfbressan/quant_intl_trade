e31f8e81f8c447696a5ab72f8b48a19b9b373b49
Rafael Bressan <rfbressan@gmail.com>
2022-02-09 16:14:21 -0300

    fix: vertical lines denoting t=0

--- EconomiaArtuc.py
+++ EconomiaArtuc.py
@@ -1,12 +1,10 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """
 Created on Thu May  2 21:47:08 2019
 
 @author: brunamirelle
 
 Modified by: Rafael Felipe Bressan (https://github.com/rfbressan)
-Date: 2021-05-14
+Date: 2022-02-09
 """
 
 import numpy as np
@@ -21,21 +19,28 @@ class EconomiaArtuc:
     beta - fator de desconto dos trabalhadores
     C - custo de mudar de setor
     K_X e K_Y = dotações de capital em cada setor
-    L_minus = dotação de trabalho na economia
+    L_bar = dotação de trabalho na economia
     p0 = preço pré-liberalização do bem X
     """
-    def __init__(self, alpha, beta, C, nu, K_X, K_Y, L_minus, p0):
+    def __init__(self, alpha, beta, C, nu, K_X, K_Y, L_bar, p0):
         self.alpha = alpha
         self.beta = beta
         self.C = C
         self.nu = nu
         self.K_X = K_X
         self.K_Y = K_Y
-        self.L_minus = L_minus
+        self.L_bar = L_bar
         self.p0 = p0
     
     #Função Omega no paper
     def Omega(self, mu):
+        """Valor da opção do trabalhador
+
+        :param mu: moving cost
+        :type mu: float
+        :return: value
+        :rtype: float
+        """
         return self.nu*np.log(1 + np.exp(mu/self.nu))
     
     #CDF da distribuição logística (diferença dos choques)
@@ -46,31 +51,33 @@ class EconomiaArtuc:
     def FOC(self, L, K, p):
         return p*self.alpha*((L)**(self.alpha-1))*((K)**(1-self.alpha))
     
-    #Deflator
+    #Indice de preços
     def phi(self,p):
         return p**(.5)
     
     #Retorna as equações necessárias para calcular steady-state
-    #vec-guess - valores, salários e thresholds (vetor)
+    #vec_guess - valores, salários e thresholds (vetor)
     #p - preço relativo do bem X no steady-state desejado
     def steady_discrep(self,vec_guess, p):
-        V_X = vec_guess[0]
-        V_Y = vec_guess[1]
-        mu_X = vec_guess[2]
-        mu_Y = vec_guess[3]
-        L_X = vec_guess[4]
-        L_Y = vec_guess[5]
-        w_X = vec_guess[6]
-        w_Y = vec_guess[7]
+        """Equações necessárias para steady-state
+
+        :param vec_guess: initial guess vector
+        :type vec_guess: ndarray of floats
+        :param p: price
+        :type p: float
+        :return: vector of values
+        :rtype: ndarray of floats
+        """
+        vx, vy, mux, muy, lx, ly, wx, wy = vec_guess
         discrep = np.zeros(8)
-        discrep[0] = w_X - self.FOC(L_X, self.K_X, p)/self.phi(p)
-        discrep[1] = w_Y - self.FOC(L_Y, self.K_Y, 1)/self.phi(p)
-        discrep[2] = L_X + L_Y - self.L_minus
-        discrep[3] = mu_X - self.beta*(V_Y-V_X) + self.C
-        discrep[4] = mu_Y - self.beta*(V_X-V_Y) + self.C
-        discrep[5] = V_X - w_X - self.beta*(V_X) - self.Omega(mu_X)
-        discrep[6] = V_Y - w_Y - self.beta*(V_Y) - self.Omega(mu_Y)
-        discrep[7] = L_X - (1-self.G(mu_X))*L_X - self.G(mu_Y)*L_Y
+        discrep[0] = wx - self.FOC(lx, self.K_X, p)/self.phi(p)
+        discrep[1] = wy - self.FOC(ly, self.K_Y, 1)/self.phi(p)
+        discrep[2] = lx + ly - self.L_bar
+        discrep[3] = mux - self.beta*(vy-vx) + self.C
+        discrep[4] = muy - self.beta*(vx-vy) + self.C
+        discrep[5] = vx - wx - self.beta*(vx) - self.Omega(mux)
+        discrep[6] = vy - wy - self.beta*(vy) - self.Omega(muy)
+        discrep[7] = lx - (1-self.G(mux))*lx - self.G(muy)*ly
         #discrep[8] = L_Y - (1-self.G(mu_Y))*L_Y - self.G(mu_X)*L_X
         return discrep  
     
@@ -80,14 +87,17 @@ class EconomiaArtuc:
             return self.steady_discrep(vec_guess, p)
         return fsolve(steady_price, np.ones(8))
     
-    """
-    Resolve o modelo para um dado valor pós-liberalização
-    p_after = preço relativo de X pós-liberalização
-    T_SS = número de períodos até novo steady-state (padrão 30, t=31 é steady-state)
-    effective_at = a partir de que período vale a abertura (padrão é o zero)?
-    tol = Tolerância para convergência do algoritmo
-    """
+ 
     def solve_model(self, p_after, T_SS = 30, effective_at = 0, tol = 1e-5):
+        """Resolve o modelo para um dado valor pós-liberalização
+        
+        :param p_after: preço relativo de X pós-liberalização
+        :param T_SS: número de períodos até novo steady-state (padrão 30, t=31 é steady-state)
+        :param effective_at: a partir de que período vale a abertura (padrão é o zero)?
+        :param tol: Tolerância para convergência do algoritmo
+        :return: time path of endogenous variables
+        :rtype: dictionary
+        """
         #Calcula valores do steady-state inicial e final
         #Inicial
         V_X_0, V_Y_0, mu_X_0, mu_Y_0, L_X_0, L_Y_0, w_X_0, w_Y_0 = self.solve_steady(self.p0)
@@ -123,7 +133,7 @@ class EconomiaArtuc:
             for tt in range(1, T_SS + 1):
                 L_X_t[tt] = (1-self.G(mu_X_t[tt-1]))*L_X_t[tt-1] +\
                 self.G(mu_Y_t[tt-1])*L_Y_t[tt-1]
-                L_Y_t[tt] = self.L_minus - L_X_t[tt]
+                L_Y_t[tt] = self.L_bar - L_X_t[tt]
                 
             w_Y_t = self.FOC(L_Y_t, self.K_Y, 1)/self.phi(p_t)
             w_X_t = self.FOC(L_X_t, self.K_X, p_t)/self.phi(p_t)
@@ -175,6 +185,11 @@ if __name__ == "__main__":
     fig1, ax1 = plt.subplots()          
     ax1.plot(sol['wx'], label = 'Sector X')
     ax1.plot(sol['wy'], label = 'Sector Y')
+    w_extended = np.append(sol['wx'], sol['wy'])
+    ax1.vlines(x=eff_at, 
+               ymin=min(w_extended),
+               ymax=max(w_extended),
+               colors='red', linestyles='dashed')
     ax1.set_title("Delayed to t = " + str(eff_at))
     ax1.set_xlabel("time")
     ax1.set_ylabel("Wages")
@@ -183,12 +198,17 @@ if __name__ == "__main__":
     fig2, ax2 = plt.subplots()
     ax2.plot(sol['Lx'], label = 'Sector X')
     ax2.plot(sol['Ly'], label = 'Sector Y')
+    l_extended = np.append(sol['Lx'], sol['Ly'])
+    ax2.vlines(x=eff_at,
+               ymin=min(l_extended),
+               ymax=max(l_extended),
+               colors='red', linestyles='dashed')
     ax2.set_title("Delayed to t = " + str(eff_at))
     ax2.set_xlabel("time")
     ax2.set_ylabel("Labor force")
     ax2.legend()
     
-    col1, col2 = st.beta_columns(2)
+    col1, col2 = st.columns(2)
     with col1:
         st.pyplot(fig1)
     with col2:
